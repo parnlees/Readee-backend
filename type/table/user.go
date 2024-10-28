cd0891f534c675c35dde5a0381977f6bc935d3c7
Joeleely <Varanyu50015@gmail.com>
2024-10-28 22:46:56 +0700

    change not null

--- type/table/user.go
+++ type/table/user.go
@@ -6,15 +6,15 @@ import (
 
 type User struct {
 	UserId       *uint64    `gorm:"primaryKey;autoIncrement"`
-	Token        *string    `gorm:"type:varchar(256) ; unique_index; not null"`
+	Token        *string    `gorm:"type:varchar(256) ; unique_index"`
 	Email        *string    `gorm:"type:VARCHAR(256); unique_index; not null"`
 	Username     *string    `gorm:"type:VARCHAR(256); unique_index; not null"`
 	Password     *string    `gorm:"type:VARCHAR(256); not null"`
-	PhoneNumber  *string    `gorm:"type:VARCHAR(256); not null"`
-	ProfileUrl   *string    `gorm:"type:VARCHAR(256); not null"`
-	Firstname    *string    `gorm:"type:VARCHAR(256); not null"`
-	Lastname     *string    `gorm:"type:VARCHAR(256); not null"`
-	Gender       *string    `gorm:"type:VARCHAR(256); not null"`
+	PhoneNumber  *string    `gorm:"type:VARCHAR(256)"`
+	ProfileUrl   *string    `gorm:"type:VARCHAR(256)"`
+	Firstname    *string    `gorm:"type:VARCHAR(256)"`
+	Lastname     *string    `gorm:"type:VARCHAR(256)"`
+	Gender       *string    `gorm:"type:VARCHAR(256)"`
 	VerifyStatus *bool      `gorm:"default:true"` // Many-to-Many relationship
 	CreatedAt    *time.Time `gorm:"autoCreateTime"`
 	UpdatedAt    *time.Time `gorm:"autoCreateTime"`
